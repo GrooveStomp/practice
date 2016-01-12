896f13b05fea94190c3ad5327cb3c594c21459f3
GrooveStomp <aaron.oman@gmail.com>
2016-01-11 22:44:55 -0800

    2016-01-11: Unix sockets

--- 2016/01/11/client.c
+++ 2016/01/11/client.c
@@ -1,24 +1,51 @@
-#include "gs_socket.c"
-#include <string.h> // strlen
+#include <string.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <fcntl.h>
+#include <errno.h>
 
 int
 main(int argc, char **argv) {
-  bool result;
 
   int socket_handle;
-  result = gs_socket_init(30000, &socket_handle);
-  if (gs_socket_error(result)) {
-    fprintf(stderr, "%s\n", gs_socket_message(result));
+  if ((socket_handle = socket(AF_UNIX, SOCK_DGRAM, 0)) == -1) {
+    fprintf(stderr, "Failed to create socket\n");
+    return 1;
+  }
+
+  const char *client_path = "/tmp/udp_sock_client";
+  struct sockaddr_un client_address;
+  memset(&client_address, 0, sizeof(struct sockaddr_un));
+  client_address.sun_family = AF_UNIX;
+  strncpy(client_address.sun_path, client_path, strlen(client_path));
+  if (bind(socket_handle, (struct sockaddr *)&client_address, sizeof(struct sockaddr_un)) == -1) {
+    fprintf(stderr, "Failed to bind socket\n");
+    return 1;
+  }
+
+  const char *server_path = "/tmp/udp_sock_server";
+  struct sockaddr_un server_address;
+  memset(&server_address, 0, sizeof(struct sockaddr_un));
+  server_address.sun_family = AF_UNIX;
+  strncpy(server_address.sun_path, server_path, strlen(server_path));
+
+  if (fcntl(socket_handle, F_SETFL, O_NONBLOCK, 1) == -1) {
+    fprintf(stderr, "Couldn't make socket non-blocking\n");
     return 1;
   }
 
   const char* packet_data = "Hello there!";
-  int packet_size = strlen(packet_data);
+  size_t packet_size = strlen(packet_data);
 
-  struct sockaddr_in address = gs_socket_address_init(30000, 127, 0, 0, 1);
-  result = gs_socket_send(socket_handle, packet_data, packet_size, address);
-  if (gs_socket_error(result)) {
-    fprintf(stderr, "%s\n", gs_socket_message(result));
+  if (sendto(socket_handle, packet_data, packet_size, 0, (struct sockaddr *)&server_address,
+             sizeof(struct sockaddr_un)) != packet_size) {
+    fprintf(stderr, "Failed to send datagram\n");
+    return 1;
+  }
+
+  if (remove(client_path) == -1 && errno != ENOENT) {
+    fprintf(stderr, "Failed to remove file: %s\n", client_path);
     return 1;
   }
 
--- 2016/01/11/server.c
+++ 2016/01/11/server.c
@@ -1,33 +1,59 @@
-#include "gs_socket.c"
-#include <string.h> // memset
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <errno.h>
+
+const char *path = "/tmp/udp_sock_server";
+
+void
+rem_file(void) {
+  if (remove(path) == -1 && errno != ENOENT) {
+    fprintf(stderr, "Failed to remove file: %s\n", path);
+  }
+}
 
 int
 main(int argc, char **argv) {
-  bool result;
+  atexit(&rem_file);
+
   uint8_t packet_data[256];
   uint32_t max_packet_size = sizeof(packet_data);
-  struct sockaddr_in from;
-  socklen_t from_length = sizeof(from);
+
+  struct sockaddr_un address;
+  memset(&address, 0, sizeof(struct sockaddr_un));
+  address.sun_family = AF_UNIX;
+  strncpy(address.sun_path, path, strlen(path));
 
   int socket_handle;
-  result = gs_socket_init(30000, &socket_handle);
-  if (gs_socket_error(result)) {
-    fprintf(stderr, "%s\n", gs_socket_message(result));
-    return 1;
+  if ((socket_handle = socket(AF_UNIX, SOCK_DGRAM, 0)) == -1) {
+    fprintf(stderr, "Failed to create socket\n");
+    exit(EXIT_FAILURE);
   }
 
-  while(true) {
-    while(true) {
+  if (bind(socket_handle, (struct sockaddr *)&address, sizeof(struct sockaddr_un)) == -1) {
+    fprintf(stderr, "Failed to bind socket\n");
+    exit(EXIT_FAILURE);
+  }
+
+  if (fcntl(socket_handle, F_SETFL, O_NONBLOCK, 1) == -1) {
+    fprintf(stderr, "Couldn't make socket non-blocking\n");
+    exit(EXIT_FAILURE);
+  }
+
+  while(1) {
+    while(1) {
       memset((void*)packet_data, 0, max_packet_size);
-      int bytes = recvfrom(socket_handle, (char*)packet_data, max_packet_size, 0, (struct sockaddr*)&from, &from_length);
-      if (bytes <= 0) break;
 
-      uint32_t from_address = ntohl(from.sin_addr.s_addr);
-      uint32_t from_port = ntohs(from.sin_port);
+      if (recvfrom(socket_handle, (char*)packet_data, max_packet_size, 0, NULL, NULL) <= 0)
+        break;
 
-      fprintf(stdout, "Recieved data: port:%d, address: %d, data: %s\n", from_port, from_address, packet_data);
+      fprintf(stdout, "Received data: %s\n", packet_data);
     }
   }
 
-  return 0;
+  exit(EXIT_SUCCESS);
 }
