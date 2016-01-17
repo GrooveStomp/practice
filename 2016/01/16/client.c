9fd7adfbad69568e8cdbd6c2999dcbd97c098baa
GrooveStomp <aaron.oman@gmail.com>
2016-01-16 21:36:04 -0800

    2016-01-16: Remove cruft from client

--- 2016/01/16/client.c
+++ 2016/01/16/client.c
@@ -1,4 +1,4 @@
-#define _SVID_SOURCE
+#define _DEFAULT_SOURCE
 #include <features.h> /* To get inet_aton() */
 
 #include <string.h>
@@ -19,13 +19,19 @@ int socket_handle;
 
 void
 close_socket(int signo) {
-  if (close(socket_handle) == -1)
+  if (close(socket_handle) == -1) {
     fprintf(stderr, "Failed to close socket\n");
+    exit(EXIT_FAILURE);
+  }
   raise(signo);
 }
 
 int
 main(int argc, char **argv) {
+  printf("+------------------------------------------------------------------------------+\n");
+  printf("|                             UDP Socket Client                                |\n");
+  printf("+------------------------------------------------------------------------------+\n");
+
   if ((socket_handle = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
     gsnet_error("Failed to create socket\n");
 
@@ -40,8 +46,6 @@ main(int argc, char **argv) {
   server.sin_family = AF_INET;
 
   char text[GSNET_PACKET_DATA_SIZE];
-  printf("UDP Socket Client Sample Program\n");
-  printf("--------------------------------\n");
 
   printf("[Client] Enter Server IPV4 address: ");
   memset(text, 0, GSNET_PACKET_DATA_SIZE);
@@ -80,35 +84,6 @@ main(int argc, char **argv) {
     printf("[Client] Sending: '%s'\n", text);
     if (sendto(socket_handle, packet, sizeof(packet), 0, (struct sockaddr *)&server, sizeof(struct sockaddr_in)) != sizeof(packet))
       gsnet_error("Failed to send datagram\n");
-
-    /* while(true) { */
-    /*   while(true) { */
-    /*     memset((void*)packet, 0, GSNET_PACKET_MAX_SIZE); */
-
-    /*     struct sockaddr_in from; */
-    /*     socklen_t from_length = sizeof(from); */
-    /*     if (recvfrom(socket_handle, (char*)packet, GSNET_PACKET_MAX_SIZE, 0, (struct sockaddr *)&from, &from_length) <= 0) */
-    /*       break; */
-
-    /*     int from_address = ntohl(from.sin_addr.s_addr); */
-    /*     int from_port = ntohs(from.sin_port); */
-
-    /*     if (memcmp(&server_connection, &gsnet_null_connection, sizeof(struct connection)) == 0) { */
-    /*       server.address = from_address; */
-    /*       server.port = from_port; */
-    /*       fprintf(stdout, "[Server] Connection initialized with IP: '%s' on port: '%d', message: '%s'\n", */
-    /*               inet_ntoa(from.sin_addr), */
-    /*               from_port, */
-    /*               gsnet_get_packet_data(packet)); */
-    /*     } */
-    /*     else if (from_address == server.address && from_port == server.port) { */
-    /*       fprintf(stdout, "[Server] Message received: '%s'\n", gsnet_get_packet_data(packet)); */
-    /*     } */
-    /*     else { */
-    /*       fprintf(stdout, "[Server] Filtered out message from IP:   '%s' on port: '%d'\n", inet_ntoa(from.sin_addr), from_port); */
-    /*     } */
-    /*   } */
-    /* } */
   }
 
   close(socket_handle);
