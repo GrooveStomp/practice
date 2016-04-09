23129e5bc117bf60b53400f33788ed8c4b38a587
GrooveStomp <aaron.oman@gmail.com>
2016-04-08 17:25:21 -0700

    2016-04-08: Introductory Rust programming examples

---
+++ 2016/04/08-2/main.rs
@@ -0,0 +1,87 @@
+fn header(section_number: &str) {
+    println!("\nSection {}:", section_number);
+    println!("--------------------------------------------------------------------------------");
+}
+
+fn s1_0_hello_world() {
+    println!("Hello, world!");
+}
+
+fn s1_2_formatting() {
+    // In general, the `{}` will be automatically replaced with any
+    // arguments. These will be stringified.
+    println!("{} days", 31);
+
+    // Without a suffix, 31 becomes an i32. You can change what type 31 is,
+    // with a suffix.
+
+    // There are various optional patterns this works with. Positional
+    // arguments can be used.
+    println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
+
+    // As can named arguments.
+    println!("{subject} {verb} {object}",
+             object="the lazy dog",
+             subject="the quick brown fox",
+             verb="jumps over");
+
+    // Special formatting can be specified after a `:`.
+    println!("{} of {:b} people know binary, the other half don't", 1, 2);
+
+    // You can right-align text with a specified width. This will output
+    // "     1". 5 white spaces and a "1".
+    println!("{number:>width$}", number=1, width=6);
+
+    // You can pad numbers with extra zeroes. This will output "000001".
+    println!("{number:>0width$}", number=1, width=6);
+
+    // It will even check to make sure the correct number of arguments are
+    // used.
+    println!("My name is {0}, {1} {0}", "James", "Bond");
+
+    // Create a structure which contains an `i32`. Name it `Structure`.
+    //    struct Structure(i32);
+
+    // However, custom types such as this structure require more complicated
+    // handling. This will not work.
+    //    println!("This struct `{}` won't print...", Structure(3));
+    // FIXME ^ Comment out this line.
+    println!("Pi is roughly {:0.3}", 22.0/7.0);
+}
+
+fn s1_2_1_debug() {
+    // Derive the `fmt::Debug` implementation for `Structure`. `Structure`
+    // is a structure which contains a single `i32`.
+    #[derive(Debug)]
+    struct Structure(i32);
+
+    // Put a `Structure` inside of the structure `Deep`. Make it printable
+    // also.
+    #[derive(Debug)]
+    struct Deep(Structure);
+
+    // Printing with `{:?}` is similar to with `{}`.
+    println!("{:?} months in a year.", 12);
+    println!("{1:?} {0:?} is the {actor:?} name.",
+             "Slater",
+             "Christian",
+             actor="actor's");
+
+    // `Structure` is printable!
+    println!("Now {:?} will print!", Structure(3));
+
+    // The problem with `derive` is there is no control over how
+    // the results look. What if I want this to just show a `7`?
+    println!("Now {:?} will print!", Deep(Structure(7)));
+}
+
+fn main() {
+    header("1.0");
+    s1_0_hello_world();
+
+    header("1.2");
+    s1_2_formatting();
+
+    header("1.2.1");
+    s1_2_1_debug();
+}
